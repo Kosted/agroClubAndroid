278d6c283e8c5d1392815197ac0837b0e8411dba
kosted <konstantinus96@gmail.com>
2018-12-13 09:19:13 +0300

    добавлена параметризация в тесты, первая попытка

--- src/test/java/tests/DitailFilterTest.java
+++ src/test/java/tests/DitailFilterTest.java
@@ -1,16 +1,46 @@
 package tests;
 
+import helperClasses.WebActions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import subPageObj.ShortMarcetPlaseDeclaration;
 import superClasses.SuperTest;
 
+import java.util.Arrays;
+import java.util.Collection;
 
+@RunWith(value = Parameterized.class)
 public class DitailFilterTest extends SuperTest {
 
+    @Parameters
+    public static Collection<Object[]> data() {
 
-    public DitailFilterTest() {
-        super(web_a);
+        Object[][] data = new Object[][]{
+
+                {"Пшеница 5 класс", "Протеин(белок), %", "11", "less"},
+                {"Рожь", "Влажность, %", "12", "more"}
+        };
+
+        return Arrays.asList(data);
+
     }
+
+    String culture;
+    String mainProperty;
+    String value;
+    String sign;
+
+    public DitailFilterTest(WebActions web_a, String culture, String mainProperty, String value, String sign) {
+        super(web_a);
+        this.culture = culture;
+        this.mainProperty = mainProperty;
+        this.value = value;
+        this.sign = sign;
+    }
+
+
 
     @Test
     public void ditailFilterTest() {
@@ -33,24 +63,23 @@ public class DitailFilterTest extends SuperTest {
 //        priceUpdatePage.clickOnConfirmButton();
 
 
-        marcetplacePage.chouseCulturefilter("Пшеница 5 класс");
+        marcetplacePage.chouseCulturefilter(culture);
 
         ShortMarcetPlaseDeclaration firstShortMarcetPlaseDeclaration = marketFilterPage.getShortMarcetPlaseDeclaration(0);
 
 
         marketFilterPage.clickOnFilterButton();
 
-        marketChousFilterPage.setFilter("Протеин(белок), %", "11", "less");
+        marketChousFilterPage.setFilter(mainProperty, value, sign);
         marketChousFilterPage.clickOnConfirmButton();
+
 
         ShortMarcetPlaseDeclaration secondShortMarcetPlaseDeclaration = marketFilterPage.getShortMarcetPlaseDeclaration(0);
 
         if (firstShortMarcetPlaseDeclaration.equals(secondShortMarcetPlaseDeclaration))
-            assertMCS.equalsTrue(Integer.parseInt(firstShortMarcetPlaseDeclaration.getMainAttribute().replaceAll("[^0-9]+",""))<=11, "была оставлена карточка не соответствующая фильтру.");
+            assertMCS.equalsTrue(Integer.parseInt(firstShortMarcetPlaseDeclaration.getMainAttribute().replaceAll("[^0-9]+", "")) <= 11, "была оставлена карточка не соответствующая фильтру.");
         else
-            assertMCS.equalsTrue(Integer.parseInt(secondShortMarcetPlaseDeclaration.getMainAttribute().replaceAll("[^0-9]+",""))<=11, "была выведена карточка не соответствующая фильтру.");
-
-
+            assertMCS.equalsTrue(Integer.parseInt(secondShortMarcetPlaseDeclaration.getMainAttribute().replaceAll("[^0-9]+", "")) <= 11, "была выведена карточка не соответствующая фильтру.");
 
 
     }
